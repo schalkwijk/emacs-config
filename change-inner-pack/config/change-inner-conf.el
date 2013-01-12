6a30fded3800aa349261fe0cdcbaa26931f99c7e
Samuel Lopes <samuelleonardo@groupon.com>
2013-01-11 23:47:10 -0600

    Added change-inner

---
+++ change-inner-pack/config/change-inner-conf.el
@@ -0,0 +1,2 @@
+(global-set-key (kbd "M-i") 'change-inner)
+(global-set-key (kbd "M-o") 'change-outer)
---
+++ change-inner-pack/init.el
@@ -0,0 +1,5 @@
+;; Multiple cursors to highlight symbol/region and modify them on the fly
+
+(live-add-pack-lib "change-inner")
+(require 'change-inner)
+(live-load-config-file "change-inner-conf.el")
